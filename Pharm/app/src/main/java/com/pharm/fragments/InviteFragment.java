b7d83d2dfc2e09ba78e40516569e7e54159a3961
AvinashChowdary <avinash.ravilla@mtuity.com>
2016-11-30 14:10:25 +0530

    removed unused files

--- Pharm/app/src/main/java/com/pharm/fragments/InviteFragment.java
+++
@@ -1,9 +0,0 @@
-package com.pharm.fragments;
-
-import android.support.v4.app.Fragment;
-
-/**
- * Created by Avinash
- */
-public class InviteFragment extends Fragment {
-}
--- UsingCircleTimer/src/com/example/foursquare/People.java
+++
@@ -1,9 +0,0 @@
-package com.example.foursquare;
-
-public class People {
-	
-	public String firstName;
-	public String lastName;
-	public String gender;
-
-}
